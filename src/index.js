770984fa6cc974f434ae020defd51cfb2087ae1f
Daniel McQuiston <dmcquiston@cricut.com>
2018-03-17 18:13:59 -0600

    Update code to pass tests

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const base = require('./lib/base');
-
+const base        = require('./lib/base');
 const definitions = require('./lib/definitions');
+const request     = require('superagent');
 
 module.exports = iComfortClient;
 
@@ -11,36 +11,67 @@ function iComfortClient (auth) {
         return new iComfortClient(auth);
     }
 
-    this.auth = Object.assign({username:null, password: null}, auth);
+    this.agent = request.agent().auth(auth.username, auth.password);
 }
 
 iComfortClient.prototype.getBuildingsInfo          = function(params) {
-    return base.doGet(definitions.getBuildingsInfo.path, this.auth, params);
+    return this.agent.get(base.getFullUri(definitions.getBuildingsInfo.path))
+        .query(params)
+        .then(resToBody)
+        .then(body => body.Buildings);
 };
 iComfortClient.prototype.getGatewayInfo            = function(params) {
-    return base.doGet(definitions.getGatewayInfo.path, this.auth, params);
+    return this.agent.get(base.getFullUri(definitions.getGatewayInfo.path))
+        .query(params)
+        .then(resToBody);
 };
 iComfortClient.prototype.getGatewaysAlerts         = function(params) {
-    return base.doGet(definitions.getGatewaysAlerts.path, this.auth, params);
+    return this.agent.get(base.getFullUri(definitions.getGatewaysAlerts.path))
+        .query(params)
+        .then(resToBody)
+        .then(body => body.Alerts);
 };
 iComfortClient.prototype.getSystemsInfo            = function(params) {
-    return base.doGet(definitions.getSystemsInfo.path, this.auth, params);
+    return this.agent.get(base.getFullUri(definitions.getSystemsInfo.path))
+        .query(params)
+        .then(resToBody)
+        .then(body => body.Systems);
 };
 iComfortClient.prototype.getThermostatInfoList     = function(params) {
-    return base.doGet(definitions.getThermostatInfoList.path, this.auth, params);
+    return this.agent.get(base.getFullUri(definitions.getThermostatInfoList.path))
+        .query(params)
+        .then(resToBody)
+        .then(body => body.tStatInfo);
 };
 iComfortClient.prototype.getThermostatLookupInfo   = function(params) {
-    return base.doGet(definitions.getThermostatLookupInfo.path, this.auth, params);
+    return this.agent.get(base.getFullUri(definitions.getThermostatLookupInfo.path))
+        .query(params)
+        .then(resToBody)
+        .then(body => body.tStatlookupInfo);
 };
 iComfortClient.prototype.getThermostatScheduleInfo = function(params) {
-    return base.doGet(definitions.getThermostatScheduleInfo.path, this.auth, params);
+    return this.agent.get(base.getFullUri(definitions.getThermostatScheduleInfo.path))
+        .query(params)
+        .then(resToBody)
+        .then(body => body.tStatScheduleInfo);
 };
 iComfortClient.prototype.validateUser              = function(params) {
-    return base.doPut(definitions.validateUser.path, this.auth, params, '');
+    return this.agent.put(base.getFullUri(definitions.validateUser.path))
+        .query(params)
+        .then(resToBody);
 };
 iComfortClient.prototype.setThermostatInfo         = function(data) {
-    return base.doPut(definitions.setThermostatInfo.path, this.auth, '', data);
+    return this.agent.put(base.getFullUri(definitions.setThermostatInfo.path))
+        .send(data)
+        .then(resToBody);
 };
-iComfortClient.prototype.setProgramMode            = function(data) {
-    return base.doDashboardPost(definitions.setModeChange.path, this.auth, '', data);
-};
+
+function resToBody (res) {
+    function Body (data) {
+        Object.assign(this,data);
+    }
+    Body.prototype.getRes = () => res;
+    Body.prototype.getReq = () => res.req;
+
+    return new Body(res.body);
+}
